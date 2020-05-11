076e824fcd9721781f4444d71e803e47144aef42
johan Bautista <jsbparra@gmail.com>
2020-05-12 01:37:51 +0200

    form component

---
+++ src/components/Formulario.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Formulario = () => {
+  return (
+    <div>
+      <form>
+        <div className="input-field col s12">
+          <input type="text" name="ciudad" id="ciudad" />
+          <label htmlFor="ciudad">Ciudad:</label>
+        </div>
+        <div className="input-field col s12">
+          <select name="pais" id="pais">
+            <option value="">-- Selecciona --</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="PE">Perú</option>
+          </select>
+          <label htmlFor="pais">Pais:</label>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;

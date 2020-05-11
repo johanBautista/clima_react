58a5817d15d9ce1e9d66fa3dccbba238c1044c03
johan Bautista <jsbparra@gmail.com>
2020-05-12 01:46:59 +0200

    usestate y handlechange

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+  //
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: '',
+  });
+  //
+  const { ciudad, pais } = busqueda;
+  //
+  const handleChange = (e) => {
+    //
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>
       <form>
         <div className="input-field col s12">
-          <input type="text" name="ciudad" id="ciudad" />
+          <input
+            type="text"
+            name="ciudad"
+            id="ciudad"
+            value={ciudad}
+            onChange={handleChange}
+          />
           <label htmlFor="ciudad">Ciudad:</label>
         </div>
         <div className="input-field col s12">
-          <select name="pais" id="pais">
+          <select name="pais" id="pais" value={pais} onChange={handleChange}>
             <option value="">-- Selecciona --</option>
             <option value="AR">Argentina</option>
             <option value="CO">Colombia</option>

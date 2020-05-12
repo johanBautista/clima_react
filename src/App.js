cc8fa2abf544fc1088e8a7442b611b5421463038
johan Bautista <jsbparra@gmail.com>
2020-05-12 10:46:29 +0200

    useEffect y consulta api fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+  //
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: '',
+  });
+  //
+  const [consultar, guardarConsultar] = useState(false);
+  //
+  const { ciudad, pais } = busqueda;
+  //
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = '4511ad99c283d8572e7c386b4622c0bf';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        console.log(resultado);
+      }
+    };
+    consultarAPI();
+  }, [consultar]);
+
   return (
     <Fragment>
       <Header titulo="ClimaReactApp" />
@@ -10,7 +33,11 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario />
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
             <div className="col m6 s12">2</div>
           </div>
